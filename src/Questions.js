eb4decb3d41550b8e6cf5f319cd6f05c90f31f87
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2022-04-28 13:47:37 -0400

    created questions structure

---
+++ src/Questions.js
@@ -0,0 +1,23 @@
+const questions = [
+  {
+    question: 'How many hours a week do you play OSRS?',
+    answerA: {
+      answer: '0-10',
+      traits: [''],
+    },
+    answerB: {
+      answer: '10-20',
+      traits: ['gamer'],
+    },
+    answerC: {
+      answer: '10-20',
+      traits: ['gamer'],
+    },
+    answerD: {
+      answer: '10-20',
+      traits: ['gamer'],
+    },
+  },
+];
+
+export default questions;
