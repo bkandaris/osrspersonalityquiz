c2a13c88e29b74dd4e3e0803747b7e85adf9314c
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2022-05-02 13:28:46 -0400

    sort working, first index and name displaying

--- src/Pages/Results.jsx
+++ src/Pages/Results.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import { useSelector} from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 const Results = () => {
   const navigate = useNavigate();
   const { Characters } = useSelector((state) => state);
-  console.log('character state in results', Characters);
+  const [result, setResult] = useState();
+
+  function compareScores(a, b) {
+    return a.score - b.score;
+  }
+
+  Characters.sort(compareScores);
+
+  console.log('result', Characters);
 
   return (
     <div>
       <h1>Results</h1>
+      <h3>{Characters[0].charName}</h3>
       <button
         onClick={() => {
           navigate('/');
